e943301ec725655ceb5788b8062b53255a9c8333
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-03-26 17:21:27 +0100

    [ADD] engine/ and all sub-directory

---
+++ engine/component/Motion.hpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_arcade_2019
+** File description:
+** Motion.hpp
+*/
+
+#ifndef OOP_ARCADE_2019_MOTION_HPP
+#define OOP_ARCADE_2019_MOTION_HPP
+
+#endif // OOP_ARCADE_2019_MOTION_HPP
