1108e4226aae9ce135a4e2297b9db2c35dce56e4
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-04-01 09:43:15 +0200

    [ADD] Add ecs base type
    - Animation
    - Vector2D

--- engine/type/Animation.hpp
+++ engine/type/Animation.hpp
@@ -8,4 +8,23 @@
 #ifndef OOP_ARCADE_2019_ANIMATION_HPP
 #define OOP_ARCADE_2019_ANIMATION_HPP
 
+namespace engine {
+
+namespace type {
+
+class Animation {
+   public:
+    explicit Animation(int row = 0, int frames = 0, int speed = 0);
+    ~Animation();
+
+   public:
+    int row;
+    int frames;
+    int speed;
+};
+
+}
+
+}
+
 #endif // OOP_ARCADE_2019_ANIMATION_HPP
