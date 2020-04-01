0f7ace4883f3388b438468b6016634a6cba85a1f
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-04-01 09:38:29 +0200

    [ADD] Add event class
    - Collision
    - Input

---
+++ engine/event/Input.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** engine
+** File description:
+** Input.cpp
+*/
+
+#include "Input.hpp"
+
+using namespace engine;
+using namespace event;
+
+Input::Input() : code(KEY_UNKNOWN)
+{
+}
+
+Input::~Input() = default;
