ad590a47615597b54596fbf445a687c652304777
Anthonin Clara <anthonin.clara@epitech.eu>
2020-04-01 14:58:26 +0200

    [ADD] game interface

---
+++ games/src/IGame.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_arcade_2019
+** File description:
+** IGame
+*/
+
+#ifndef IGAME_HPP_
+#define IGAME_HPP_
+
+#include <string>
+
+namespace game {
+    class IGame {
+        public:
+            virtual ~IGame() = 0;
+    
+            virtual std::string getName() = 0;
+    
+            virtual void init() = 0;
+            virtual void destroy() = 0;
+        protected:
+        private:
+    };
+};
+
+#endif /* !IGAME_HPP_ */
