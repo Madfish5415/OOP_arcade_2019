d721fb3f562f75f11c75b4a909271a388091d471
lucas <lucas.fabreguettes@epitech.eu>
2020-04-02 12:18:04 +0200

    [ADD] create.cpp for SFML
    Add a function to create the Graphical object of SFML

---
+++ lib/sfml/src/create.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** sfml
+** File description:
+** createSFML.cpp
+*/
+
+#include "../../../engine/eventbus/EventBus.hpp"
+#include "../../graphical/IGraphical.hpp"
+#include "graphical/Graphical.hpp"
+
+extern "C" graphical::IGraphical* create(engine::eventbus::EventBus& eventbus) {
+    return new sfml::Graphical(eventbus);
+}
