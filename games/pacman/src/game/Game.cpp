1ea428452f1095b2a56a36562ff2a254c2547104
Anthonin Clara <anthonin.clara@epitech.eu>
2020-04-05 16:18:42 +0200

    [MODIF] map pacman

--- games/pacman/src/game/Game.cpp
+++ games/pacman/src/game/Game.cpp
@@ -42,20 +42,20 @@ Game::~Game()
 
 void Game::init()
 {
-    std::vector<std::pair<int, int>> wallsPosition = {{0, 0}, {40, 0}, {80, 0}, {120, 0}, {160, 0}, {200, 0}, {240, 0}, {280, 0}, {320, 0},
-        {360, 0}, {400, 0}, {440, 0}, {480, 0}, {520, 0}, {560, 0}, {600, 0}, {640, 0}, {680, 0}, {720, 0}, {760, 0}, {800, 0}, {840, 0},
-        {880, 0}, {920, 0}, {960, 0}, {1000, 0}, {1040, 0}, {1080, 0}, {1120, 0}, {1160, 0}, {1200, 0}, {1240, 0}, {1280, 0}, {1320, 0},
-        {1360, 0}, {1400, 0}, {1440, 0}, {1480, 0}, {1520, 0}, {1560, 0}, {1600, 0}, {1640, 0}, {1680, 0}, {1720, 0}, {1760, 0}, {1800, 0},
-        {1840, 0}, {1880, 0}, {0, 1040}, {40, 1040}, {80, 1040}, {120, 1040}, {160, 1040}, {200, 1040}, {240, 1040}, {280, 1040},
-        {320, 1040}, {360, 1040}, {400, 1040}, {440, 1040}, {480, 1040}, {520, 1040}, {560, 1040}, {600, 1040}, {640, 1040}, {680, 1040},
-        {720, 1040}, {760, 1040}, {800, 1040}, {840, 1040}, {880, 1040}, {920, 1040}, {960, 1040}, {1000, 1040}, {1040, 1040}, {1080, 1040},
-        {1120, 1040}, {1160, 1040}, {1200, 1040}, {1240, 1040}, {1280, 1040}, {1320, 1040}, {1360, 1040}, {1400, 1040}, {1440, 1040},
-        {1480, 1040}, {1520, 1040}, {1560, 1040}, {1600, 1040}, {1640, 1040}, {1680, 1040}, {1720, 1040}, {1760, 1040}, {1800, 1040},
-        {1840, 1040}, {1880, 1040}, {0, 40}, {1880, 40}, {0, 80}, {1880, 80}, {0, 120}, {1880, 120}, {0, 160}, {1880, 160}, {0, 200},
-        {1880, 200}, {0, 240}, {1880, 240}, {0, 280}, {1880, 280}, {0, 320}, {1880, 320}, {0, 360}, {1880, 360}, {0, 400}, {1880, 400},
-        {0, 440}, {1880, 440}, {0, 480}, {1880, 480}, {0, 520}, {1880, 520}, {0, 560}, {1880, 560}, {0, 600}, {1880, 600}, {0, 640},
-        {1880, 640}, {0, 680}, {1880, 680}, {0, 720}, {1880, 720}, {0, 760}, {1880, 760}, {0, 800}, {1880, 800}, {0, 840}, {1880, 840},
-        {0, 880}, {1880, 880}, {0, 920}, {1880, 920}, {0, 960}, {1880, 960}, {0, 1000}, {1880, 1000}, {0, 1040}, {1880, 1040}};
+    std::vector<std::pair<int, int>> wallsPosition = {{480, 270}, {520, 270}, {560, 270}, {600, 270}, {640, 270}, {680, 270}, {720, 270}, {760, 270}, {800, 270}, {840, 270}, {880, 270}, {920, 270}, {960, 270}, {1000, 270}, {1040, 0}, {1080, 270}, {1120, 270}, {1160, 270}, {1200, 270}, {1240, 270}, {1280, 270}, {1320, 270}, {1360, 270}, {1400, 270}, {1440, 270},
+                                                        {480, 790}, {520, 790}, {560, 790}, {600, 790}, {640, 790}, {680, 790}, {720, 790}, {760, 790}, {800, 790}, {840, 790}, {880, 790}, {920, 790}, {960, 790}, {1000, 790}, {1040, 790}, {1080, 790}, {1120, 790}, {1160, 790}, {1200, 790}, {1240, 790}, {1280, 790}, {1320, 790}, {1360, 790}, {1400, 790}, {1440, 790},
+                                                        {480, 310}, {1440, 310}, {480, 350}, {1440, 350}, {480, 390}, {1440, 390}, {480, 430}, {1440, 430}, {480, 470}, {1440, 470}, {480, 510}, {1440, 510}, {480, 550}, {1440, 550}, {480, 590}, {1440, 590}, {480, 630}, {1440, 630}, {480, 670}, {1440, 670}, {480, 710}, {1440, 710}, {480, 750}, {1440, 750},
+                                                        {560, 350}, {600, 350}, {680, 350}, {720, 350}, {800, 350}, {840, 350}, {880, 350}, {920, 350}, {1000, 350}, {1040, 350}, {1080, 350}, {1120, 350}, {1200, 350}, {1240, 350}, {1320, 350}, {1360, 350},
+                                                        {560, 390}, {600, 390}, {680, 390}, {1240, 390}, {1320, 390}, {1360, 390},
+                                                        {680, 430}, {760, 430}, {840, 430}, {880, 430}, {920, 430}, {1000, 430}, {1040, 430}, {1080, 430}, {1160, 430}, {1240, 430},
+                                                        {520, 470}, {560, 470}, {600, 470}, {760, 470}, {840, 470}, {1080, 470}, {1160, 470}, {1320, 470}, {1360, 470}, {1400, 470},
+                                                        {680, 510}, {840, 510}, {1080, 510}, {1240, 510},
+                                                        {680, 550}, {760, 550}, {840, 550}, {1080, 550}, {1160, 550}, {1240, 550},
+                                                        {520, 590}, {560, 590}, {600, 590}, {760, 590}, {840, 590}, {1080, 590}, {1160, 590}, {1320, 590}, {1360, 590}, {1400, 590},
+                                                        {680, 630}, {840, 630}, {880, 630}, {920, 630}, {960, 630}, {1000, 630}, {1040, 630}, {1080, 630}, {1240, 630},
+                                                        {560, 670}, {600, 670}, {680, 670}, {720, 670}, {760, 670}, {1160, 670}, {1200, 670}, {1240, 670}, {1320, 670}, {1360, 670},
+                                                        {560, 710}, {760, 710}, {800, 710}, {840, 710}, {880, 710}, {920, 710}, {1000, 710}, {1040, 710}, {1080, 710}, {1120, 710}, {1160, 710}, {1360, 710},
+                                                        {640, 750}, {680, 750}, {1240, 750}, {1280, 750}};
 
 
     engine::ecs::World& mainGame = this->_universe.createWorld("mainGamePacman");
@@ -71,7 +71,7 @@ void Game::init()
     pacmanEntity.addComponent<engine::component::Hitbox>(40, 40);
     pacmanEntity.addComponent<engine::component::Motion>(engine::type::Vector2D(1, 0), engine::type::Vector2D(0, 0));
     pacmanEntity.addComponent<engine::component::Size>(40, 40);
-    pacmanEntity.addComponent<engine::component::Transform>(engine::type::Vector2D(40, 40), 3);
+    pacmanEntity.addComponent<engine::component::Transform>(engine::type::Vector2D(960, 670), 3);
     pacmanEntity.addComponent<component::User>();
 
     engine::ecs::Entity& redGhost = mainGame.createEntity();
@@ -80,7 +80,7 @@ void Game::init()
     redGhost.addComponent<engine::component::Hitbox>(40, 40);
     redGhost.addComponent<engine::component::Motion>(engine::type::Vector2D(0, 1), engine::type::Vector2D(0, 0));
     redGhost.addComponent<engine::component::Size>(40, 40);
-    redGhost.addComponent<engine::component::Transform>(engine::type::Vector2D(200, 400), 2);
+    redGhost.addComponent<engine::component::Transform>(engine::type::Vector2D(920, 470), 2);
     redGhost.addComponent<component::AI>();
 
     engine::ecs::Entity& blueGhost = mainGame.createEntity();
@@ -89,7 +89,7 @@ void Game::init()
     blueGhost.addComponent<engine::component::Hitbox>(40, 40);
     blueGhost.addComponent<engine::component::Motion>(engine::type::Vector2D(0, 1), engine::type::Vector2D(0, 0));
     blueGhost.addComponent<engine::component::Size>(40, 40);
-    blueGhost.addComponent<engine::component::Transform>(engine::type::Vector2D(500, 800), 2);
+    blueGhost.addComponent<engine::component::Transform>(engine::type::Vector2D(1040, 470), 2);
     blueGhost.addComponent<component::AI>();
 
     engine::ecs::Entity& pinkGhost = mainGame.createEntity();
@@ -98,7 +98,7 @@ void Game::init()
     pinkGhost.addComponent<engine::component::Hitbox>(40, 40);
     pinkGhost.addComponent<engine::component::Motion>(engine::type::Vector2D(0, 1), engine::type::Vector2D(0, 0));
     pinkGhost.addComponent<engine::component::Size>(40, 40);
-    pinkGhost.addComponent<engine::component::Transform>(engine::type::Vector2D(1400, 300), 2);
+    pinkGhost.addComponent<engine::component::Transform>(engine::type::Vector2D(880, 550), 2);
     pinkGhost.addComponent<component::AI>();
 
     engine::ecs::Entity& orangeGhost = mainGame.createEntity();
@@ -107,7 +107,7 @@ void Game::init()
     orangeGhost.addComponent<engine::component::Hitbox>(40, 40);
     orangeGhost.addComponent<engine::component::Motion>(engine::type::Vector2D(0, 1), engine::type::Vector2D(0, 0));
     orangeGhost.addComponent<engine::component::Size>(40, 40);
-    orangeGhost.addComponent<engine::component::Transform>(engine::type::Vector2D(1100, 900), 2);
+    orangeGhost.addComponent<engine::component::Transform>(engine::type::Vector2D(1000, 550), 2);
     orangeGhost.addComponent<component::AI>();
 
     const std::vector<std::string> pathsWall {"assets/wall.png", "assets/wall.png", "ressource_wall"};
